0ef38a36a9f85f86873d243b0337a0d68b9180e6
Sean Mercer <Sean@ool-ad0201ae.dyn.optonline.net>
2019-03-07 12:19:33 -0500

    Basic game and formatting done

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const names = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
 const suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
-const values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
+const values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 13]
 const deck = []
 let playerHand = []
 let dealerHand = []
@@ -23,30 +23,53 @@ const buildDeck = function() {
       }
 //Image addition syntax provided by https://codereview.stackexchange.com/questions/153251/single-player-repeatable-blackjack-game
 
-const dealCardsToPlayers = function() {
+const dealCardsToPlayer = function() {
   for (let i=0; i < 2; i++) {
       let dealtHand = Math.floor(Math.random() * deck.length)
       let card = deck[dealtHand]
       playerHand.push(card)
       deck.splice(dealtHand, 1)
-      let faceUpCard = document.createElement('img')
-      faceUpCard.setAttribute('src', playerHand[i].Image)
-      faceUpCard.setAttribute('class', 'playerCards')
-      player.appendChild(faceUpCard)
-    
-      let dealtHandTwo = Math.floor(Math.random() * deck.length)
+  }
+  renderPlayerCards()
+}
+
+const dealCardsToDealer = function() {
+  for (let i = 0; i < 2; i++) {
+    let dealtHandTwo = Math.floor(Math.random() * deck.length)
       let cardTwo = deck[dealtHandTwo]
       dealerHand.push(cardTwo)
       deck.splice(dealtHandTwo, 1)
-      let faceDownCard = document.createElement('img')
+  }
+  renderDealerCards()
+}
+
+const renderPlayerCards = function() {
+for (let i = 0; i < 2; i++) {
+  let faceUpCard = document.createElement('img')
+  faceUpCard.setAttribute('src', playerHand[i].Image)
+  faceUpCard.setAttribute('class', 'playerCards')
+  player.appendChild(faceUpCard)
+}}
+
+const renderDealerCards = function() {
+  for (let i=0; i < 2; i++){
+  let faceDownCard = document.createElement('img')
       faceDownCard.setAttribute('src', 'CSS/Assets/card_back_red.png')
       faceDownCard.setAttribute('class', 'dealerCards')
       dealer.appendChild(faceDownCard)
-      blackjack()
-  }
+}}
+
+const toggleButtonsPostDeal = function() {
   dealButton.disabled = true
   hitButton.disabled = false
   stayButton.disabled = false
+}
+
+const dealCards = function () {
+  dealCardsToPlayer()
+  dealCardsToDealer()
+  blackjack()
+  toggleButtonsPostDeal()
 }
 
  const handValue = function(hand) {
@@ -56,92 +79,112 @@ const dealCardsToPlayers = function() {
     }
     return total
   }
-
   
   const blackjack = function () {
     if (handValue(dealerHand) === 21) {
+      disableGameButtons()
       alert("It's a push!")
       revealDealerCards()
     } else if (handValue(playerHand) === 21) {
+      disableGameButtons()
       alert("Blackjack! Player wins!")
       revealDealerCards()
     } else if (handValue(dealerHand) === 21) {
+      disableGameButtons()
       alert("Blackjack! House wins!")
       revealDealerCards()
     }
   }
 
-const dealerHit = function () {
+const playerAddCard = function() {
+  let dealtCard = Math.floor(Math.random() * deck.length)
+    let card = deck[dealtCard]
+    playerHand.push(card)
+    deck.splice(dealtCard, 1)
+    renderPlayerHit()
+}
+
+const renderPlayerHit = function() {
+  for (let i = playerHand.length - 1; i < playerHand.length; i++) {
+    let faceUpCard = document.createElement('img')
+    faceUpCard.setAttribute('src', playerHand[i].Image)
+    faceUpCard.setAttribute('class', 'playerCards')
+    player.appendChild(faceUpCard)
+  }
+}
+
+const hitMe = function() {
+  playerAddCard()
+  blackjack()
+  bust()
+}
+
+const dealerAddCard = function () {
     let dealtCard = Math.floor(Math.random() * deck.length)
     let card = deck[dealtCard]
     dealerHand.push(card)
     deck.splice(dealtCard, 1)
-    for (let i = dealerHand.length -1; i < dealerHand.length; i++) {
-      let faceDownCard = document.createElement('img')
-      faceDownCard.setAttribute('src', 'CSS/Assets/card_back_red.png')
-      faceDownCard.setAttribute('class', 'dealerCards')
-      dealer.appendChild(faceDownCard)
+    renderDealerHit()
     }
-    blackjack()
-    bust()
+
+const renderDealerHit = function () {
+  for (let i = dealerHand.length -1; i < dealerHand.length; i++) {
+    let faceDownCard = document.createElement('img')
+    faceDownCard.setAttribute('src', 'CSS/Assets/card_back_red.png')
+    faceDownCard.setAttribute('class', 'dealerCards')
+    dealer.appendChild(faceDownCard)
+  }
+}
+
+const dealerHit = function() {
+  dealerAddCard()
+  blackjack()
+  bust()
 }
 
 const bust = function() {
   if (handValue(playerHand) > 21) {
-    aceHighOrLow(playerHand)
-    hitButton.disabled = true
-    stayButton.disabled = true
+    disableGameButtons()
     alert("Player Busts! House Wins!")
     revealDealerCards() 
   } else if (handValue(dealerHand) > 21) {
-    aceHighOrLow(dealerHand)
-    hitButton.disabled = true
-    stayButton.disabled = true
+    disableGameButtons()
     alert("House Busts! Player Wins!")
     revealDealerCards()
   }
 }
 
+const disableGameButtons = function() {
+  dealButton.disabled = true
+  hitButton.disabled = true
+  stayButton.disabled = true
+}
+
 const checkForWinner = function() {
   if (handValue(playerHand) > handValue(dealerHand) && handValue(playerHand) < 21) {
-    aceHighOrLow(dealerHand)
-    aceHighOrLow(playerHand)
-      hitButton.disabled = true
-      stayButton.disabled = true
       alert("Player wins!")
       revealDealerCards() 
   } else if (handValue(playerHand) < handValue(dealerHand) && handValue(dealerHand) < 21) {
-    aceHighOrLow(dealerHand)
-    aceHighOrLow(playerHand)
-      hitButton.disabled = true
-      stayButton.disabled = true
       alert("House wins!")
       revealDealerCards()
   } else if (handValue(playerHand) === handValue(dealerHand)) {
-    aceHighOrLow(dealerHand)
-    aceHighOrLow(playerHand)
-      hitButton.disabled = true
-      stayButton.disabled = true
       alert("It's a push!")
       revealDealerCards()
   }
 }
 
-const dealerTurn = function () {
-  aceHighOrLow(dealerHand)
+const toggleButtonsDealerTurn = function() {
   hitButton.disabled = true
   stayButton.disabled = true
-  if (handValue(dealerHand) <= 16) {
-    dealerHit()
-    if (handValue(dealerHand) <= 16) {
-      dealerHit()
-      if (handValue(dealerHand) <= 16) {
-        dealerHit()
-      } 
-    }
-  }
-  checkForWinner()
 }
+
+const dealerTurn = function () {
+  toggleButtonsDealerTurn()
+  while (handValue(dealerHand) < 17) {
+    dealerHit()
+  }
+    checkForWinner()
+  }
 
 const revealDealerCards = function () {
   let dealerCards = document.querySelectorAll('.dealerCards')
@@ -150,21 +193,8 @@ const revealDealerCards = function () {
 }}
 
 const playGame = function() {
-  dealButton.addEventListener('click', dealCardsToPlayers)
-  hitButton.addEventListener('click', function() {
-    let dealtCard = Math.floor(Math.random() * deck.length)
-    let card = deck[dealtCard]
-    playerHand.push(card)
-    deck.splice(dealtCard, 1)
-    for (let i = playerHand.length - 1; i < playerHand.length; i++) {
-      let faceUpCard = document.createElement('img')
-      faceUpCard.setAttribute('src', playerHand[i - 1].Image)
-      faceUpCard.setAttribute('class', 'playerCards')
-      player.appendChild(faceUpCard)
-    }
-    blackjack()
-    bust()
-  })
+  dealButton.addEventListener('click', dealCards)
+  hitButton.addEventListener('click', hitMe)
   stayButton.addEventListener('click', function() {
     dealerTurn()
   })  
@@ -174,12 +204,3 @@ playGame()
 newHand.addEventListener('click', function() {
   window.location.reload()
 }) 
-
-
-const aceHighOrLow = function(hand) {
-  for (let i = 0; i < hand.length; i++) {
-    if(handValue(hand) > 21 && hand[i].Name === "Ace") {
-      handValue(hand) -= 10
-    }
-  }
-}
